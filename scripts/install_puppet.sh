aee2e044ac935f6dc1d9273fef23f4e00969a960
Harry Owen <howen@skyscapecloud.com>
2016-10-26 13:54:37 +0100

    Refine Puppet Agent Installation script

--- scripts/install_puppet.sh
+++ scripts/install_puppet.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
+export PUPPETMASTER_HOSTNAME=$1
+export DISTRO=$(cat /proc/version)
+if [[ $DISTRO =~ 'Debian' ]]; then
+  echo "Installing cURL..."
+  apt -qq install curl > /dev/null 2>&1
+fi
 echo "Installing Puppet..."
 (
-  PUPPETMASTER_HOSTNAME=vagrant-puppetmaster.vagrant.local
   nslookup $PUPPETMASTER_HOSTNAME > /dev/null
   if [ $? -eq 0 ]; then
-    echo "installing from local puppetmaster..."
-    grep Debian /etc/issue
-    if [ $? -eq 0 ]; then
-      echo "...but first we need cURL. This is Debian, after all..."
-      apt -q -y install curl > /dev/null
-    fi
+    echo "Installing from local puppetmaster..."
     curl -sk "https://${PUPPETMASTER_HOSTNAME}:8140/packages/current/install.bash" | bash > /dev/null
   else
-    echo "no puppetmaster found, installing from yum repo..."
-    rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm && yum install puppet -y > /dev/null
+    echo "No puppetmaster found, installing from repository..."
+    UPDATE_MSGS=(
+      "Acquiring repository package for Puppet Community Edition"
+      "Installing Puppet Community Edition"
+      "Symlinking"
+      "Done."
+    )
+
+    PUPPET_LOCATION=/opt/puppetlabs/bin/puppet
+    if [[ $DISTRO =~ "Ubuntu" || $DISTRO =~ "Debian" ]]; then
+      RELEASE=$(lsb_release -a 2> /dev/null | grep Codename | awk '{print $2}')
+      echo "${UPDATE_MSGS[0]}..."
+      curl -s http://apt.puppetlabs.com/puppetlabs-release-pc1-${RELEASE}.deb -o /tmp/puppet_repo.deb
+      echo "${UPDATE_MSGS[1]}..."
+      dpkg -i /tmp/puppet_repo.deb > /dev/null && rm /tmp/puppet_repo.deb
+      apt -qq update > /dev/null 2>&1 && apt -qq install puppet-agent > /dev/null 2>&1
+    elif [[ $DISTRO =~ "Red Hat" ]]; then
+      RELEASE='el-6'
+      echo "${UPDATE_MSGS[0]}..."
+      rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm > /dev/null 2>&1
+      echo "${UPDATE_MSGS[1]}..." 
+      yum install puppet -y > /dev/null 2>&1
+    fi
   fi
+  echo "${UPDATE_MSGS[2]}..."
+  ln -s ${PUPPET_LOCATION} /usr/bin/puppet
+  echo "${UPDATE_MSGS[3]}"
 )
